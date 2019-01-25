2f3237311209c8b10f344b1d262c87b34855766c
bcliang <brad@bliang.com>
2019-01-25 13:25:49 -0800

    implement usage.py example

--- usage.py
+++ usage.py
@@ -1,7 +1,9 @@
-import dash_extendable_graph
+import dash_extendable_graph as deg
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_html_components as html
+import dash_core_components as dcc
+import random
 
 app = dash.Dash(__name__)
 
@@ -9,17 +11,43 @@ app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
 
 app.layout = html.Div([
-    dash_extendable_graph.ExtendableGraph(
-        id='input',
-        value='my-value',
-        label='my-label'
+    deg.ExtendableGraph(
+        id='extendablegraph_example',
+        config={'showAxisDragHandles': True,
+                'showAxisRangeEntryBoxes': True,
+                'modeBarButtonsToRemove': [
+                    'sendDataToCloud',
+                    'lasso2d',
+                    'autoScale2d',
+                    'hoverClosestCartesian',
+                    'hoverCompareCartesian',
+                    'toggleSpikelines'],
+                'displaylogo': False,
+                },
+        figure=dict(
+            data=[{'x': [0],
+                   'y': [0],
+                   'mode':'lines+markers'
+                   }],
+        )
     ),
+    dcc.Interval(
+        id='interval_extendablegraph_update',
+        interval=1000,
+        n_intervals=0,
+        max_intervals=-1),
     html.Div(id='output')
 ])
 
-@app.callback(Output('output', 'children'), [Input('input', 'value')])
-def display_output(value):
-    return 'You have entered {}'.format(value)
+
+@app.callback(Output('extendablegraph_example', 'extendData'),
+              [Input('interval_extendablegraph_update', 'n_intervals')],
+              [State('extendablegraph_example', 'figure'),
+               State('extendablegraph_example', 'extendData')])
+def update_extendData(n_intervals, existing, info):
+    x_new = existing['data'][0]['x'][-1] + 1
+    y_new = random.random()
+    return [dict(x=[x_new], y=[y_new])]
 
 
 if __name__ == '__main__':
