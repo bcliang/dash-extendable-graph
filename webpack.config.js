d4ffe0aad5cd7d3ec7e92304304da1aabb3cdc26
bcliang <brad@bliang.com>
2019-07-02 08:01:59 -0700

    Revert "sync to component-boilerplate"
    This reverts commit ca3757dd3bbf086880f2bd64ffc0d507cb53173b.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const packagejson = require('./package.json');
 const dashLibraryName = packagejson.name.replace(/-/g, '_');
 
 module.exports = (env, argv) => {
+
     let mode;
 
     const overrides = module.exports || {};
@@ -24,24 +25,22 @@ module.exports = (env, argv) => {
     }
 
     let filename = (overrides.output || {}).filename;
-    if (!filename) {
+    if(!filename) {
         const modeSuffix = mode === 'development' ? 'dev' : 'min';
         filename = `${dashLibraryName}.${modeSuffix}.js`;
     }
 
     const entry = overrides.entry || {main: './src/lib/index.js'};
 
-    const devtool = overrides.devtool || 'source-map';
+    const devtool = overrides.devtool || (
+        mode === 'development' ? "eval-source-map" : 'none'
+    );
 
-    const externals =
-        'externals' in overrides
-            ? overrides.externals
-            : {
-                  react: 'React',
-                  'react-dom': 'ReactDOM',
-                  'plotly.js': 'Plotly',
-                  'prop-types': 'PropTypes',
-              };
+    const externals = ('externals' in overrides) ? overrides.externals : ({
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'plotly.js': 'Plotly',
+    });
 
     return {
         mode,
@@ -52,12 +51,11 @@ module.exports = (env, argv) => {
             library: dashLibraryName,
             libraryTarget: 'window',
         },
-        devtool,
         externals,
         module: {
             rules: [
                 {
-                    test: /\.jsx?$/,
+                    test: /\.js$/,
                     exclude: /node_modules/,
                     use: {
                         loader: 'babel-loader',
@@ -68,9 +66,6 @@ module.exports = (env, argv) => {
                     use: [
                         {
                             loader: 'style-loader',
-                            options: {
-                                insertAt: 'top',
-                            },
                         },
                         {
                             loader: 'css-loader',
@@ -79,5 +74,6 @@ module.exports = (env, argv) => {
                 },
             ],
         },
-    };
+        devtool
+    }
 };
--- webpack.serve.config.js
+++ webpack.serve.config.js
@@ -1,11 +1,7 @@
 const config = require('./webpack.config.js');
-const path = require('path');
 
 config.entry = {main: './src/demo/index.js'};
-config.output = {
-    filename: './output.js',
-    path: path.resolve(__dirname),
-};
+config.output = {filename: 'output.js'};
 config.mode = 'development';
 config.externals = undefined; // eslint-disable-line
 config.devtool = 'inline-source-map';
