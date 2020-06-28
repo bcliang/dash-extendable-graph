c4cb01bb5c88af20baf246596197f60351a3c77f
Brad Liang <brad@bliang.com>
2020-06-28 13:14:14 -0700

    cleanup docs

--- tests/assets/clientside-method.js
+++ tests/assets/clientside-method.js
@@ -3,18 +3,25 @@ if (!window.dash_clientside) {
 }
 
 if (!window.GRAPH_DIV_ID) {
+    // the component id as specified in the integratio ntest
     window.GRAPH_DIV_ID = "deg-clientside-test";
 }
 
 window.dash_clientside.pytest = {
+    
+    /** 
+     * pytest.relayout() this clientside function attempts to append 
+     * "-new" onto the specified figure's title. 
+     * 
+     * Testing for changed text is easy to implement w/ dash[testing]
+     * similar checks could be done for other props of figure.layout
+     * 
+     **/
     relayout: function(button_click, fig) {
-        console.log(fig)
-        // update only values within nested objects
         const update = {
             "title.text": fig.layout.title + "-new"
         };
 
-        //console.log("relayout: ", update);
         Plotly.relayout(window.GRAPH_DIV_ID, update);
 
         return true;
