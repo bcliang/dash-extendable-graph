ef2666c03b1e456dae43eb6b4df6778a6432efc9
Brad Liang <brad@bliang.com>
2021-04-08 23:08:07 -0700

    fix clientside test, cleanup test formatting via black

--- tests/test_clientside_callbacks.py
+++ tests/test_clientside_callbacks.py
@@ -5,28 +5,23 @@ import dash
 import dash_html_components as html
 import dash_extendable_graph as deg
 from dash.dependencies import Input, Output, State, ClientsideFunction
-from dash.exceptions import PreventUpdate
 
 
 def test_extg007_clientside(dash_duo):
     app = dash.Dash(__name__)
     initial_graph_title = "initial title"
     header = html.Div(
-        id='header',
-        children=[html.Button(id='update-title', children=['Update Title'])],
+        id="header",
+        children=[html.Button(id="update-title", children=["Update Title"])],
     )
     graph = html.Div(
         children=[
             deg.ExtendableGraph(
-                id='deg-clientside-test',
+                id="deg-clientside-test",
                 figure=dict(
                     layout=dict(title=initial_graph_title),
                     data=[
-                        dict(
-                            x=[1, 2, 3, 4],
-                            y=[5, 4, 3, 6],
-                            line=dict(shape='spline'),
-                        )
+                        dict(x=[1, 2, 3, 4], y=[5, 4, 3, 6], line=dict(shape="spline"),)
                     ],
                 ),
             )
@@ -34,18 +29,18 @@ def test_extg007_clientside(dash_duo):
     )
 
     app.clientside_callback(
-        ClientsideFunction('pytest', 'relayout'),
-        Output('header', 'style'),
-        [Input('update-title', 'n_clicks')],
-        [State('deg-clientside-test', 'figure')],
+        ClientsideFunction("pytest", "relayout"),
+        Output("header", "style"),
+        [Input("update-title", "n_clicks")],
+        [State("deg-clientside-test", "figure")],
     )
 
-    app.layout = html.Div(
-        [header, graph]
-    )
+    app.layout = html.Div([header, graph])
 
     dash_duo.start_server(app)
 
-    dash_duo.wait_for_contains_text('#deg-clientside-test', initial_graph_title)
-    dash_duo.wait_for_element('#update-title').click()
-    dash_duo.wait_for_contains_text('#deg-clientside-test', "{}-new".format(initial_graph_title))
+    dash_duo.wait_for_contains_text("#deg-clientside-test", initial_graph_title)
+    dash_duo.wait_for_element("#update-title").click()
+    dash_duo.wait_for_contains_text(
+        "#deg-clientside-test", "{}-new".format(initial_graph_title)
+    )
--- tests/test_extend_trace_selectively.py
+++ tests/test_extend_trace_selectively.py
@@ -15,44 +15,47 @@ import json
 
 def test_extg004_extend_trace_selectively(dash_duo):
     app = dash.Dash(__name__)
-    app.layout = html.Div([
-        deg.ExtendableGraph(id='extend_trace_selectively',
-                            figure=dict(data=[
-                                dict(y=[0]),
-                                dict(y=[1]),
-                            ])),
-        html.Div(id='output'),
-        dcc.Interval(
-            id='interval_extendablegraph_update',
-            interval=100,
-            n_intervals=0,
-            max_intervals=1)
-    ])
+    app.layout = html.Div(
+        [
+            deg.ExtendableGraph(
+                id="extend_trace_selectively",
+                figure=dict(data=[dict(y=[0]), dict(y=[1]),]),
+            ),
+            html.Div(id="output"),
+            dcc.Interval(
+                id="interval_extendablegraph_update",
+                interval=100,
+                n_intervals=0,
+                max_intervals=1,
+            ),
+        ]
+    )
 
-    @app.callback(Output('extend_trace_selectively', 'extendData'),
-                  [Input('interval_extendablegraph_update', 'n_intervals')],
-                  [State('extend_trace_selectively', 'figure')])
+    @app.callback(
+        Output("extend_trace_selectively", "extendData"),
+        [Input("interval_extendablegraph_update", "n_intervals")],
+        [State("extend_trace_selectively", "figure")],
+    )
     def trace_will_extend_selectively(n_intervals, figure):
         if n_intervals is None or n_intervals < 1:
             raise PreventUpdate
 
         return [dict(y=[2])], [1]
 
-    @app.callback(Output('output', 'children'),
-                  [Input('extend_trace_selectively', 'extendData')],
-                  [State('extend_trace_selectively', 'figure')])
+    @app.callback(
+        Output("output", "children"),
+        [Input("extend_trace_selectively", "extendData")],
+        [State("extend_trace_selectively", "figure")],
+    )
     def display_data(trigger, figure):
         if figure is None:
             raise PreventUpdate
 
-        return json.dumps(figure['data'])
+        return json.dumps(figure["data"])
 
     dash_duo.start_server(app)
     dash_duo.find_element("#extend_trace_selectively")
 
-    comparison = json.dumps(
-        [dict(y=[0]),
-         dict(y=[1, 2])]
-    )
+    comparison = json.dumps([dict(y=[0]), dict(y=[1, 2])])
 
-    dash_duo.wait_for_text_to_equal('#output', comparison)
+    dash_duo.wait_for_text_to_equal("#output", comparison)
