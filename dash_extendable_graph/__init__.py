4fa78e77adc80b796f07ae667ae2fb9ab6d2fa74
Brad Liang <brad@bliang.com>
2021-04-09 08:20:13 -0700

    codestyle for component __init__

--- dash_extendable_graph/__init__.py
+++ dash_extendable_graph/__init__.py
@@ -10,19 +10,22 @@ import dash as _dash
 from ._imports_ import *
 from ._imports_ import __all__
 
-if not hasattr(_dash, 'development'):
-    print('Dash was not successfully imported. '
-          'Make sure you don\'t have a file '
-          'named \n"dash.py" in your current directory.', file=_sys.stderr)
+if not hasattr(_dash, "development"):
+    print(
+        "Dash was not successfully imported. "
+        "Make sure you don't have a file "
+        'named \n"dash.py" in your current directory.',
+        file=_sys.stderr,
+    )
     _sys.exit(1)
 
 _basepath = _os.path.dirname(__file__)
-_filepath = _os.path.abspath(_os.path.join(_basepath, 'package.json'))
+_filepath = _os.path.abspath(_os.path.join(_basepath, "package.json"))
 with open(_filepath) as f:
     package = json.load(f)
 
-package_name = package['name'].replace(' ', '_').replace('-', '_')
-__version__ = package['version']
+package_name = package["name"].replace(" ", "_").replace("-", "_")
+__version__ = package["version"]
 
 _current_path = _os.path.dirname(_os.path.abspath(__file__))
 
@@ -31,18 +34,19 @@ _this_module = _sys.modules[__name__]
 
 _js_dist = [
     {
-        'relative_package_path': 'dash_extendable_graph.min.js',
-        'dev_package_path': 'dash_extendable_graph.dev.js',
-        'external_url': 'https://unpkg.com/{0}@{1}/{0}/{2}.min.js'.format(
-            package_name, __version__, __name__),
-        'namespace': package_name
+        "relative_package_path": "dash_extendable_graph.min.js",
+        "dev_package_path": "dash_extendable_graph.dev.js",
+        "external_url": "https://unpkg.com/{0}@{1}/{0}/{2}.min.js".format(
+            package_name, __version__, __name__
+        ),
+        "namespace": package_name,
     },
     {
-        'relative_package_path': 'plotly.min.js',
-        'external_url': (
-            'https://unpkg.com/{0}@{1}/{0}/plotly.min.js'
-        ).format(package_name, __version__),
-        'namespace': package_name,
+        "relative_package_path": "plotly.min.js",
+        "external_url": ("https://unpkg.com/{0}@{1}/{0}/plotly.min.js").format(
+            package_name, __version__
+        ),
+        "namespace": package_name,
     },
 ]
 
@@ -50,5 +54,5 @@ _css_dist = []
 
 
 for _component in __all__:
-    setattr(locals()[_component], '_js_dist', _js_dist)
-    setattr(locals()[_component], '_css_dist', _css_dist)
+    setattr(locals()[_component], "_js_dist", _js_dist)
+    setattr(locals()[_component], "_css_dist", _css_dist)
